bfdc8b07fb0e8848e7ceb54ecdbb8526d89ad697
dexploitdm <knutdmit@gmail.com>
2019-06-22 21:10:55 +0500

    add datime navbar

--- src/main.js
+++ src/main.js
@@ -2,12 +2,14 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import dateFilter from '@/filters/date.filter'
 import 'materialize-css/dist/js/materialize'
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
+Vue.filter('date', dateFilter);
 
 new Vue({
   router,
   store,
   render: h => h(App)
-}).$mount('#app')
+}).$mount('#app');
