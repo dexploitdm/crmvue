a9d13a717001da73ff46c86133aa2892421b712f
Дмитрий Паршаков <knutdmit@yandex.ru>
2019-06-23 13:30:53 +0500

    messages logout

--- src/main.js
+++ src/main.js
@@ -5,10 +5,12 @@ import router from './router'
 import store from './store'
 import dateFilter from '@/filters/date.filter'
 import 'materialize-css/dist/js/materialize'
+import messagePlugin from '@/utils/message.plugin'
 
 Vue.config.productionTip = false;
 Vue.filter('date', dateFilter);
 Vue.use(Vuelidate);
+Vue.use(messagePlugin);
 
 new Vue({
   router,
--- src/views/Login.vue
+++ src/views/Login.vue
@@ -53,6 +53,7 @@
 </template>
 <script>
     import {email, required, minLength} from 'vuelidate/lib/validators'
+    import messages from '@/utils/messages'
     export  default {
         data() {
             return {
@@ -69,6 +70,11 @@
             password: {
                 required,
                 minLength: minLength(6)
+            }
+        },
+        mounted() {
+            if(messages[this.$route.query.message]){
+                this.$message(messages[this.$route.query.message])
             }
         },
         methods: {
